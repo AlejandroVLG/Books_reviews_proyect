0051a348bb90ba43b981e5138cbc1600c4511bd5
Alexlg <Alejandrolg86@gmail.com>
2022-08-08 10:58:48 +0200

    feat: user migration created

---
+++ database/migrations/2022_08_08_084403_create_users_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('lastName');
+            $table->string('nickName');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->string('gender');
+            $table->integer('age');
+            $table->string('country');
+            $table->string('favourite_author');
+            $table->string('favourite_genre');
+            $table->string('currently_readying');
+            $table->string('facebook_account')->unique();
+            $table->string('twitter_account')->unique();
+            $table->string('instagram_account')->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+};
