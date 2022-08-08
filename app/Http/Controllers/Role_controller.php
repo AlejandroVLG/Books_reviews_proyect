6b3c18e83ddfc01b8ab70e8f88a17cfe6cfed427
Alexlg <Alejandrolg86@gmail.com>
2022-08-08 12:12:22 +0200

    feat: user/review/role/book models created

---
+++ app/Http/Controllers/Role_controller.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Role_controller extends Controller
+{
+    //
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -56,4 +56,19 @@ class User extends Authenticatable implements JWTSubject
     {
         return [];
     }
+
+    public function roles()
+    {
+        return $this->belongsToMany(Role::class);
+    }
+
+    public function books()
+    {
+        return $this->hasMany(Book::class);
+    }
+
+    public function reviews()
+    {
+        return $this->hasMany(Review::class);
+    }
 }
