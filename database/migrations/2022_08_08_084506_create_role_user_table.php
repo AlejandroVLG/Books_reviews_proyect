42cca263851e982da0f5add714ba6f3b49bf69b8
Alexlg <Alejandrolg86@gmail.com>
2022-08-08 11:02:00 +0200

    feat: roles and role_user migrations created

--- database/migrations/2022_08_08_084506_create_role_user_table.php
+++ database/migrations/2022_08_08_084506_create_role_user_table.php
@@ -15,7 +15,13 @@ return new class extends Migration
     {
         Schema::create('role_user', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('role_id');
+            $table->unsignedBigInteger('user_id');
+            $table->unique(['role_id', 'user_id']);
             $table->timestamps();
+
+            $table->foreign('role_id')->references('id')->on('roles')->onDelete('cascade');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
         });
     }
 
