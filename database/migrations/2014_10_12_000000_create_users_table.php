199371f3030095fec1952be01344db1cf5579879
Alexlg <Alejandrolg86@gmail.com>
2022-08-08 11:18:48 +0200

    fix: users migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,10 +16,19 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('last_name');
+            $table->string('nick_name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
+            $table->string('gender');
+            $table->integer('age');
+            $table->string('country');
+            $table->string('favourite_author');
+            $table->string('favourite_genre');
+            $table->string('currently_readying');
+            $table->string('facebook_account')->unique();
+            $table->string('twitter_account')->unique();
+            $table->string('instagram_account')->unique();
             $table->timestamps();
         });
     }
