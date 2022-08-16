c7522bdc5c87e901abc7471ecb4896c2019a6d29
Alexlg <Alejandrolg86@gmail.com>
2022-08-16 12:22:13 +0200

    fix: auth controller currently_reading field naming

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -63,10 +63,10 @@ class AuthController extends Controller
             ]);
 
             $users = User::all();
-            
+
             // Por defecto se asigna al primer usuario creado los roles "Admin" y "Super_Admin", apartir de ahí
             // a todos los demás usarios se le asignará automáticamente el role de "User"
-            if (count($users) == 0) {
+            if (count($users) == 1) {
 
                 $user->roles()->attach(self::ROLE_ADMIN);
                 $user->roles()->attach(self::ROLE_SUPER_ADMIN);
